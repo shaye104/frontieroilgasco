f7465e4c138fc59adce60d7365099f34e0af05d5
shaye104 <accounts@sooijen.com>
2026-02-27 15:49:09 +0100

    Fix onboarding hidden-state layout and bust stale script cache

--- assets/js/pages/onboarding-page.js
+++ assets/js/pages/onboarding-page.js
@@ -1,4 +1,4 @@
-import { getOnboardingBootstrap, submitOnboarding, verifyOnboardingRoblox } from '../modules/admin-api.js?v=20260227b';
+import { getOnboardingBootstrap, submitOnboarding, verifyOnboardingRoblox } from '../modules/admin-api.js?v=20260227c';
 import { performLogout } from '../modules/nav.js?v=20260227a';
 import { clearMessage, showMessage } from '../modules/notice.js?v=20260227a';
 
