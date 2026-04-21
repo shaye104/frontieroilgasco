35c66d315b28d936115ea62be34735cd63cbf199
Shaye <shaye104@users.noreply.github.com>
2026-04-21 07:07:44 +0200

    Stabilize finance chart proportions

--- assets/js/pages/finances-page.js
+++ assets/js/pages/finances-page.js
@@ -691,7 +691,7 @@ function renderCartesianLineChart(target, lines, options = {}) {
 
   target.innerHTML = `
     <div class="finance-chart-shell">
-      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none" aria-hidden="true">
+      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="xMidYMid meet" aria-hidden="true">
         <rect x="${plotLeft}" y="${plotTop}" width="${plotWidth}" height="${plotHeight}" fill="#ffffff"></rect>
         ${yGrid}
         <line class="finance-axis-line" x1="${plotLeft}" y1="${plotBottom}" x2="${plotRight}" y2="${plotBottom}"></line>
@@ -831,7 +831,7 @@ function renderCartesianBarChart(target, series, label, color, options = {}) {
 
   target.innerHTML = `
     <div class="finance-chart-shell">
-      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none" aria-hidden="true">
+      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="xMidYMid meet" aria-hidden="true">
         <rect x="${plotLeft}" y="${plotTop}" width="${plotWidth}" height="${plotHeight}" fill="#ffffff"></rect>
         ${yGrid}
         <line class="finance-axis-line" x1="${plotLeft}" y1="${plotBottom}" x2="${plotRight}" y2="${plotBottom}"></line>
@@ -940,7 +940,7 @@ function renderCartesianHorizontalBarChart(target, series, label, color, options
 
   target.innerHTML = `
     <div class="finance-chart-shell">
-      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none" aria-hidden="true">
+      <svg class="finance-cartesian-svg" viewBox="0 0 ${width} ${height}" preserveAspectRatio="xMidYMid meet" aria-hidden="true">
         <rect x="${plotLeft}" y="${plotTop}" width="${plotWidth}" height="${plotHeight}" fill="#ffffff"></rect>
         ${xGrid}
         <line class="finance-axis-line" x1="${plotLeft}" y1="${plotBottom}" x2="${plotRight}" y2="${plotBottom}"></line>
